2e8a864ac00241a418d137f296784a969a90e607
evicno <229501549+evicno@users.noreply.github.com>
2026-02-18 17:15:07 +0100

    Add weather icons and error message

--- src/modules/domManager.js
+++ src/modules/domManager.js
@@ -1,24 +1,35 @@
 import { getWeatherData } from './weatherManager.js';
+import unknownIcon from '../icons/unknown.svg';
 
 const weatherCard = document.querySelector('.weather');
 
 export const renderApp = () => {
   const form = document.querySelector('form');
   const input = document.querySelector('#location');
+  const button = document.querySelector('button');
+
   // Reset the form and display
   weatherCard.style.display = 'none';
   form.reset();
 
   form.addEventListener('submit', (event) => {
+    weatherCard.style.display = 'none';
     event.preventDefault();
+    button.disabled = true;
     const location = input.value.trim();
+    const message = document.querySelector('.message');
+    message.textContent = '';
 
     getWeatherData(location)
       .then((weatherData) => {
         renderWeather(weatherData);
+        button.disabled = false;
       })
       .catch((error) => {
         console.log(error);
+        message.textContent = 'Location not found. Please try again!';
+        weatherCard.style.display = 'none';
+        button.disabled = false;
       });
   });
 };
@@ -26,19 +37,27 @@ export const renderApp = () => {
 function renderWeather(weather) {
   const city = document.querySelector('.city');
   const conditions = document.querySelector('.conditions');
-  //   const time = document.querySelector('.time');
+  const icon = document.querySelector('.icon img');
   const temperature = document.querySelector('.temperature');
   const feelsLike = document.querySelector('.feels-like');
   const humidity = document.querySelector('.humidity');
-  //   const precipitation = document.querySelector('.precipitation');
 
   weatherCard.style.display = 'flex';
   city.textContent = capitalizeCity(weather.city);
   conditions.textContent = weather.conditions;
+
+  // Get icon
+  console.log(weather.icon);
+  const weatherIcon = weather.icon;
+  import(`../icons/${weatherIcon}.svg`)
+    .then((ic) => (icon.src = ic.default))
+    .catch(() => (icon.src = unknownIcon));
+
   temperature.textContent = weather.temp + ' °F';
+
   feelsLike.textContent = 'Feels like ' + weather.feelsLike + ' °F';
+
   humidity.textContent = 'Humidity: ' + weather.humidity + ' %';
-  //   precipitation.textContent
 }
 
 function capitalizeCity(city) {
--- src/modules/weatherManager.js
+++ src/modules/weatherManager.js
@@ -5,15 +5,13 @@ export async function getWeatherData(location) {
     const data = await getLocationData(location);
 
     const city = data.address;
-    const time = data.currentConditions.datetime;
     const conditions = data.currentConditions.conditions;
     const feelsLike = data.currentConditions.feelslike;
     const humidity = data.currentConditions.humidity;
     const icon = data.currentConditions.icon;
-    const precip = data.currentConditions.precip;
     const temp = data.currentConditions.temp;
 
-    return { city, time, temp, precip, humidity, feelsLike, conditions, icon };
+    return { city, temp, humidity, feelsLike, conditions, icon };
   } catch (error) {
     console.log(error);
   }
