bde6d37b914617042b7503fc8b2c5ebffbfa7df8
evicno <229501549+evicno@users.noreply.github.com>
2026-02-18 20:12:16 +0100

    Add README.md

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import './styles/reset.css';
 import './styles/main.css';
-import { getWeatherData } from './modules/weatherManager.js';
-import { getLocationData } from './modules/api.js';
+
 import { renderApp } from './modules/domManager.js';
 
-getLocationData('angers').then((response) => console.log(response));
 renderApp();
